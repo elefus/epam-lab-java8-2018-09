8a5f7b51f11a01c626908c48bd11df47aaa38baa
elefus <elefus@yandex.ru>
2018-09-19 23:28:59 +0300

    Added lambda.part1.exercise3

---
+++ src/test/java/lambda/part1/exercise/Exercise3.java
@@ -0,0 +1,62 @@
+package lambda.part1.exercise;
+
+import lambda.data.Person;
+import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
+import java.util.List;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.is;
+import static org.hamcrest.Matchers.arrayContaining;
+
+@SuppressWarnings({"ConstantConditions", "unused"})
+class Exercise3 {
+
+    @Test
+    void sortPersonsByAgeUsingArraysSortExpressionLambda() {
+        Person[] persons = getPersons();
+
+        // TODO use Arrays.sort + expression-lambda
+
+        assertThat(persons, is(arrayContaining(
+                new Person("Иван", "Мельников", 20),
+                new Person("Николай", "Зимов", 30),
+                new Person("Алексей", "Доренко", 40),
+                new Person("Артем", "Зимов", 45)
+        )));
+    }
+
+    @Test
+    void sortPersonsByLastNameThenFirstNameUsingArraysSortExpressionLambda() {
+        Person[] persons = getPersons();
+
+        // TODO use Arrays.sort + statement-lambda
+
+        assertThat(persons, is(arrayContaining(
+                new Person("Алексей", "Доренко", 40),
+                new Person("Артем", "Зимов", 45),
+                new Person("Николай", "Зимов", 30),
+                new Person("Иван", "Мельников", 20)
+        )));
+    }
+
+    @Test
+    void findFirstWithAge30UsingGuavaPredicateLambda() {
+        List<Person> persons = Arrays.asList(getPersons());
+
+        // TODO use FluentIterable
+        Person person = null;
+
+        assertThat(person, is(new Person("Николай", "Зимов", 30)));
+    }
+
+    private Person[] getPersons() {
+        return new Person[]{
+                new Person("Иван", "Мельников", 20),
+                new Person("Алексей", "Доренко", 40),
+                new Person("Николай", "Зимов", 30),
+                new Person("Артем", "Зимов", 45)
+        };
+    }
+}
