05f7c5f9e69d72d37f0636dde4f7d8e3415dddae
Dgafar <nithogg@rambler.ru>
2018-09-23 15:50:56 +0300

    Task 2 exercise1 completed

--- src/test/java/lambda/part1/exercise/Exercise2.java
+++ src/test/java/lambda/part1/exercise/Exercise2.java
@@ -23,21 +23,26 @@ class Exercise2 {
 
     @Test
     void implementsIntegerMultiplierUsingAnonymousClass() {
-        Multiplier<Integer> multiplier = null;
+        Multiplier<Integer> multiplier = new Multiplier<Integer>() {
+            @Override
+            public Integer multiply(Integer value, int multiplier) {
+                return value*multiplier;
+            }
+        };
 
         testIntegerMultiplier(multiplier);
     }
 
     @Test
     void implementsMultiplierUsingStatementLambda() {
-        Multiplier<Integer> multiplier = null;
+        Multiplier<Integer> multiplier = (value, multiplier1) -> {return value* multiplier1;};
 
         testIntegerMultiplier(multiplier);
     }
 
     @Test
     void implementsIntegerMultiplierUsingExpressionLambda() {
-        Multiplier<Integer> multiplier = null;
+        Multiplier<Integer> multiplier = (value, multiplier1) -> value* multiplier1;
 
         testIntegerMultiplier(multiplier);
     }
@@ -60,7 +65,7 @@ class Exercise2 {
 
     @Test
     void implementsStringMultiplierUsingClassMethodReference() {
-        Multiplier<String> multiplier = null;
+        Multiplier<String> multiplier = Exercise2::multiplyString;
 
         assertThat(multiplier.multiply("a", 3), is("aaa"));
         assertThat(multiplier.multiply("qwerty", 0), is(emptyString()));
@@ -81,7 +86,7 @@ class Exercise2 {
 
     @Test
     void implementsStringMultiplierUsingObjectMethodReference() {
-        Multiplier<String> multiplier = null;
+        Multiplier<String> multiplier = this::stringSumWithDelimiter;
 
         assertThat(multiplier.multiply("a", 3), is("a-a-a"));
         assertThat(multiplier.multiply("qwerty", 0), is(emptyString()));
