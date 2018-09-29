a7bf9c6b796bc1e3fe4ea3e7d01ac08fa8ac578f
elefus <elefus@yandex.ru>
2018-09-29 11:01:55 +0300

    Added a puzzler to the lambdas

---
+++ src/test/java/lambda/part1/example/Example13.java
@@ -0,0 +1,25 @@
+package lambda.part1.example;
+
+import org.junit.jupiter.api.Test;
+
+@SuppressWarnings("all")
+class Example13 {
+
+    @Test
+    void go1() {
+        Talkative talkative = () -> System.out.println(say());
+        talkative.run();
+    }
+
+    private String say() {
+        return "Bonjour";
+    }
+}
+
+@SuppressWarnings("all")
+interface Talkative extends Runnable {
+
+    default String say() {
+        return "Hello!";
+    }
+}
