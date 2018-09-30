a5d1254f13f7925f458e52df8c53d4b6d6f87596
Aleksandr Burachenko <ss47305@gmail.com>
2018-09-30 12:26:00 +0300

    Part3.Exercise4: task done.

--- src/test/java/lambda/part3/exercise/Exercise4.java
+++ src/test/java/lambda/part3/exercise/Exercise4.java
@@ -1,15 +1,14 @@
 package lambda.part3.exercise;
 
+import com.google.common.primitives.Chars;
 import lambda.data.Employee;
 import lambda.data.JobHistoryEntry;
 import lambda.data.Person;
 import org.hamcrest.Matchers;
+import org.jetbrains.annotations.NotNull;
 import org.junit.jupiter.api.Test;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.List;
+import java.util.*;
 import java.util.function.Function;
 
 import static org.hamcrest.MatcherAssert.assertThat;
@@ -19,20 +18,36 @@ class Exercise4 {
 
     private static class LazyCollectionHelper<T, R> {
 
-        public static <T> LazyCollectionHelper<T, T> from(List<T> list) {
-            throw new UnsupportedOperationException();
+        private List<T> source;
+        private Function<T, List<R>> function;
+
+        public LazyCollectionHelper(@NotNull List<T> list, @NotNull Function<T, List<R>> function) {
+            this.source = list;
+            this.function = function;
+        }
+
+        public static <T> LazyCollectionHelper<T, T> from(@NotNull List<T> list) {
+            return new LazyCollectionHelper<>(list, Collections::singletonList);
         }
 
         public <U> LazyCollectionHelper<T, U> flatMap(Function<R, List<U>> flatMapping) {
-            throw new UnsupportedOperationException();
+            return new LazyCollectionHelper<>(source, unify(flatMapping).compose(function));
         }
 
         public <U> LazyCollectionHelper<T, U> map(Function<R, U> mapping) {
-            throw new UnsupportedOperationException();
+            return new LazyCollectionHelper<>(source, unify(mapping.andThen(Collections::singletonList)).compose(function));
         }
 
         public List<R> force() {
-            throw new UnsupportedOperationException();
+            return unify(function).apply(source);
+        }
+
+        private <U, V> Function<List<U>, List<V>> unify(Function<U, List<V>> function) {
+            return list -> {
+                List<V> result = new ArrayList<>();
+                list.forEach(function.andThen(result::addAll)::apply);
+                return result;
+            };
         }
     }
 
@@ -40,17 +55,16 @@ class Exercise4 {
     void mapEmployeesToCodesOfLetterTheirPositionsUsingLazyFlatMapHelper() {
         List<Employee> employees = getEmployees();
 
-        List<Integer> codes = null;
-        // TODO              LazyCollectionHelper.from(employees)
-        // TODO                                  .flatMap(Employee -> JobHistoryEntry)
-        // TODO                                  .map(JobHistoryEntry -> String(position))
-        // TODO                                  .flatMap(String -> Character(letter))
-        // TODO                                  .map(Character -> Integer(code letter)
-        // TODO                                  .force();
+        List<Integer> codes = LazyCollectionHelper.from(employees)
+                                                  .flatMap(Employee::getJobHistory)
+                                                  .map(JobHistoryEntry::getPosition)
+                                                  .flatMap(position -> Chars.asList(position.toCharArray()))
+                                                  .map(Integer::valueOf)
+                                                  .force();
         assertThat(codes, Matchers.contains(calcCodes("dev", "dev", "tester", "dev", "dev", "QA", "QA", "dev", "tester", "tester", "QA", "QA", "QA", "dev").toArray()));
     }
 
-    private static List<Integer> calcCodes(String...strings) {
+    private static List<Integer> calcCodes(String... strings) {
         List<Integer> codes = new ArrayList<>();
         for (String string : strings) {
             for (char letter : string.toCharArray()) {
@@ -85,7 +99,7 @@ class Exercise4 {
                 new Employee(
                         new Person("Анна", "Светличная", 21),
                         Collections.singletonList(
-                                new JobHistoryEntry(1, "tester", "T-Systems")
+                                new JobHistoryEntry(1, "tester", "U-Systems")
                         )),
                 new Employee(
                         new Person("Игорь", "Толмачёв", 50),
@@ -96,7 +110,7 @@ class Exercise4 {
                 new Employee(
                         new Person("Иван", "Александров", 33),
                         Arrays.asList(
-                                new JobHistoryEntry(2, "QA", "T-Systems"),
+                                new JobHistoryEntry(2, "QA", "U-Systems"),
                                 new JobHistoryEntry(3, "QA", "EPAM"),
                                 new JobHistoryEntry(1, "dev", "EPAM")
                         ))
