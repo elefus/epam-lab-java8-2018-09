b10884b969d50fd03c65f960369cd749479ca31b
Andrei Dedov <Andrei_Dedov@epam.com>
2018-10-02 09:30:56 +0300

    Very meaningful comment

--- src/test/java/lambda/part3/exercise1/Exercise1.java
+++ src/test/java/lambda/part3/exercise1/Exercise1.java
@@ -9,6 +9,7 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
+import java.util.function.Function;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
@@ -21,10 +22,20 @@ public class Exercise1 {
         List<Integer> lengths = new ArrayList<>();
 
         // TODO функция извлечения информации о человеке из объекта сотрудника personExtractor: Employee -> Person
+        Function<Employee, Person> personExtractor = Employee::getPerson;
+
         // TODO функция извлечения полного имени из информации о человеке fullNameExtractor: Person -> String
+        Function<Person, String> fullNameExtractor = Person::getFullName;
+
         // TODO функция извлечения длины из строки stringLengthExtractor: String -> Integer
+        Function<String, Integer> stringLengthExtractor = String::length;
+
         // TODO функция извлечения длины полного имени из сотрудника fullNameLengthExtractor: Employee -> Integer
+        Function<Employee, Integer> fullNameLengthExtractor = personExtractor.andThen(fullNameExtractor)
+                                                                             .andThen(stringLengthExtractor);
+
         // TODO преобразование списка employees в lengths используя fullNameLengthExtractor
+        employees.forEach(employee -> lengths.add(fullNameLengthExtractor.apply(employee)));
 
         assertEquals(Arrays.asList(14, 19, 14, 15, 14, 16), lengths);
     }
