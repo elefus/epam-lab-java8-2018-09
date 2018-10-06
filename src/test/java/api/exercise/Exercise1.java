92c9998026b95cad79eaa3b4ebf67a046e997893
Vasilii_Tolkachev <trustNOone1>
2018-10-06 16:47:17 +0300

    homework.api.exercise1

--- src/test/java/api/exercise/Exercise1.java
+++ src/test/java/api/exercise/Exercise1.java
@@ -7,6 +7,8 @@ import org.junit.jupiter.api.Test;
 import java.util.HashMap;
 import java.util.Map;
 
+import static api.exercise.Exercise1.Status.ACCEPTED;
+import static api.exercise.Exercise1.Status.DECLINED;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.hasEntry;
 import static org.hamcrest.Matchers.hasKey;
@@ -34,10 +36,10 @@ class Exercise1 {
         candidates.put(ivan, Status.PENDING);
         candidates.put(helen, Status.PENDING);
 
-        // TODO implementation
+        candidates.replaceAll((key, value) -> key.getAge() > 21 ? ACCEPTED : DECLINED);
 
-        assertThat(candidates, Matchers.hasEntry(ivan, Status.ACCEPTED));
-        assertThat(candidates, Matchers.hasEntry(helen, Status.ACCEPTED));
+        assertThat(candidates, Matchers.hasEntry(ivan, ACCEPTED));
+        assertThat(candidates, Matchers.hasEntry(helen, ACCEPTED));
         assertThat(candidates, Matchers.hasEntry(alex, Status.DECLINED));
     }
 
@@ -55,10 +57,11 @@ class Exercise1 {
         candidates.put(new Person("b", "c", 16), Status.PENDING);
         candidates.put(new Person("b", "c", 5), Status.PENDING);
 
-        // TODO implementation
+        candidates.entrySet().removeIf(entry -> entry.getKey().getAge() < 21);
+        candidates.replaceAll((key,value) -> ACCEPTED);
 
-        assertThat(candidates, Matchers.hasEntry(ivan, Status.ACCEPTED));
-        assertThat(candidates, Matchers.hasEntry(helen, Status.ACCEPTED));
+        assertThat(candidates, Matchers.hasEntry(ivan, ACCEPTED));
+        assertThat(candidates, Matchers.hasEntry(helen, ACCEPTED));
         assertThat(candidates, not(hasKey(alex)));
     }
 
@@ -71,11 +74,9 @@ class Exercise1 {
         candidates.put(alex, Status.PENDING);
         candidates.put(ivan, Status.PENDING);
 
-        // TODO implementation
-
-        Status alexStatus = null;
-        Status ivanStatus = null;
-        Status helenStatus = null;
+        Status alexStatus = candidates.getOrDefault(alex, Status.UNKNOWN);
+        Status ivanStatus = candidates.getOrDefault(ivan, Status.UNKNOWN);
+        Status helenStatus = candidates.getOrDefault(helen, Status.UNKNOWN);
 
         assertThat(alexStatus, is(Status.PENDING));
         assertThat(ivanStatus, is(Status.PENDING));
@@ -91,16 +92,16 @@ class Exercise1 {
         Map<Person, Status> oldValues = new HashMap<>();
         oldValues.put(alex, Status.PENDING);
         oldValues.put(dmitry, Status.DECLINED);
-        oldValues.put(ivan, Status.ACCEPTED);
+        oldValues.put(ivan, ACCEPTED);
 
         Map<Person, Status> newValues = new HashMap<>();
         newValues.put(alex, Status.DECLINED);
         newValues.put(helen, Status.PENDING);
 
-        // TODO implementation
+        oldValues.forEach(newValues::putIfAbsent);
 
         assertThat(newValues, hasEntry(alex, Status.DECLINED));
-        assertThat(newValues, hasEntry(ivan, Status.ACCEPTED));
+        assertThat(newValues, hasEntry(ivan, ACCEPTED));
         assertThat(newValues, hasEntry(helen, Status.PENDING));
     }
 }
