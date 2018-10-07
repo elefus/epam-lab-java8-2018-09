e2ac6edf6599ccf5d63389dbbafcc12bf904b16f
Dmitry <euroshow.light.demyanov@gmail.com>
2018-10-07 18:00:51 +0300

    api ex 1 done

--- src/test/java/api/exercise/Exercise1.java
+++ src/test/java/api/exercise/Exercise1.java
@@ -34,7 +34,7 @@ class Exercise1 {
         candidates.put(ivan, Status.PENDING);
         candidates.put(helen, Status.PENDING);
 
-        // TODO implementation
+        candidates.replaceAll((person, status) -> person.getAge() > 21? Status.ACCEPTED : Status.DECLINED);
 
         assertThat(candidates, Matchers.hasEntry(ivan, Status.ACCEPTED));
         assertThat(candidates, Matchers.hasEntry(helen, Status.ACCEPTED));
@@ -55,7 +55,8 @@ class Exercise1 {
         candidates.put(new Person("b", "c", 16), Status.PENDING);
         candidates.put(new Person("b", "c", 5), Status.PENDING);
 
-        // TODO implementation
+        candidates.replaceAll((person, status) -> person.getAge() > 21? Status.ACCEPTED : Status.DECLINED);
+        candidates.entrySet().removeIf(personStatusEntry -> personStatusEntry.getValue().equals(Status.DECLINED));
 
         assertThat(candidates, Matchers.hasEntry(ivan, Status.ACCEPTED));
         assertThat(candidates, Matchers.hasEntry(helen, Status.ACCEPTED));
@@ -71,11 +72,9 @@ class Exercise1 {
         candidates.put(alex, Status.PENDING);
         candidates.put(ivan, Status.PENDING);
 
-        // TODO implementation
-
-        Status alexStatus = null;
-        Status ivanStatus = null;
-        Status helenStatus = null;
+        Status alexStatus = candidates.getOrDefault(alex, Status.UNKNOWN);
+        Status ivanStatus = candidates.getOrDefault(ivan, Status.UNKNOWN);
+        Status helenStatus = candidates.getOrDefault(helen, Status.UNKNOWN);
 
         assertThat(alexStatus, is(Status.PENDING));
         assertThat(ivanStatus, is(Status.PENDING));
@@ -97,7 +96,7 @@ class Exercise1 {
         newValues.put(alex, Status.DECLINED);
         newValues.put(helen, Status.PENDING);
 
-        // TODO implementation
+        oldValues.forEach(newValues::putIfAbsent);
 
         assertThat(newValues, hasEntry(alex, Status.DECLINED));
         assertThat(newValues, hasEntry(ivan, Status.ACCEPTED));
