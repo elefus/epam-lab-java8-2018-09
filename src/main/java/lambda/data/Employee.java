fd41337e2b5b3aea619d1681b06cb115a41d842c
Sarvarov Sergey <sarvarov.s@mail.ru>
2018-10-08 23:01:46 +0300

    Update Employee.java

--- src/main/java/lambda/data/Employee.java
+++ src/main/java/lambda/data/Employee.java
@@ -1,49 +1,64 @@
 package lambda.data;
 
-import java.util.ArrayList;
-import java.util.List;
+import org.jetbrains.annotations.NotNull;
+
+import java.io.Serializable;
 import java.util.Objects;
 
-public class Employee {
+public class Person implements Serializable {
 
-    private final Person person;
-    private final List<JobHistoryEntry> jobHistory;
+    private final String firstName;
+    private final String lastName;
+    private final int age;
 
-    public Employee(Person person, List<JobHistoryEntry> jobHistory) {
-        this.person = person;
-        this.jobHistory = new ArrayList<>(jobHistory);
+    public Person(String firstName, String lastName, int age) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.age = age;
     }
 
-    public Person getPerson() {
-        return person;
+    public Person() {
+        firstName = "default";
+        lastName = "default";
+        age = -1;
     }
 
-    public List<JobHistoryEntry> getJobHistory() {
-        return new ArrayList<>(jobHistory);
+    public String getFirstName(Person this) {
+        return firstName;
+    }
+
+    public String getLastName(Person this) {
+        return this.lastName;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public String getFullName() {
+        return firstName + " " + lastName;
     }
 
     @Override
-    public String toString() {
-        return "Employee@" + hashCode() + " {"
-             + "person=" + person + ", "
-             + "jobHistory=" + jobHistory + "}";
-    }
-
-    @Override
-    public boolean equals(Object object) {
-        if (this == object) {
-            return true;
-        }
-        if (object == null || getClass() != object.getClass()) {
-            return false;
-        }
-        Employee employee = (Employee) object;
-        return Objects.equals(person, employee.person)
-            && Objects.equals(jobHistory, employee.jobHistory);
+    public boolean equals(Object other) {
+        if (this == other) return true;
+        if (other == null || getClass() != other.getClass()) return false;
+        Person person = (Person) other;
+        return age == person.age
+            && Objects.equals(firstName, person.firstName)
+            && Objects.equals(lastName, person.lastName);
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(person, jobHistory);
+        return Objects.hash(age, firstName, lastName);
+    }
+
+    @Override
+    public String toString() {
+        return "Person@" + hashCode() + ": {"
+             + "firstName='" + firstName + "', "
+             + "lastName='" + lastName + "', "
+             + "age=" + age + "}";
     }
 }
