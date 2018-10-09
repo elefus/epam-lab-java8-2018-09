eaef66b4e972f3ecf39bfb5132ec2e2ac4c3e519
Sergei Sarvarov <sergei_sarvarov@epam.com>
2018-10-09 23:40:35 +0300

    Part3 Exercise 4 is done

--- src/test/java/lambda/part3/exercise/Exercise4.java
+++ src/test/java/lambda/part3/exercise/Exercise4.java
@@ -6,10 +6,7 @@ import lambda.data.Person;
 import org.hamcrest.Matchers;
 import org.junit.jupiter.api.Test;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.List;
+import java.util.*;
 import java.util.function.Function;
 
 import static org.hamcrest.MatcherAssert.assertThat;
@@ -19,20 +16,38 @@ class Exercise4 {
 
     private static class LazyCollectionHelper<T, R> {
 
+        List<T> sourceList;
+        Function<T, List<R>> function;
+
+        public LazyCollectionHelper(List<T> sourceList, Function<T, List<R>> function) {
+            this.sourceList = Objects.requireNonNull(sourceList);
+            this.function = Objects.requireNonNull(function);
+        }
+
         public static <T> LazyCollectionHelper<T, T> from(List<T> list) {
-            throw new UnsupportedOperationException();
+            return new LazyCollectionHelper<>(list, Collections::singletonList);
         }
 
         public <U> LazyCollectionHelper<T, U> flatMap(Function<R, List<U>> flatMapping) {
-            throw new UnsupportedOperationException();
+            return new LazyCollectionHelper<>(sourceList, listTransformation(flatMapping).compose(function));
         }
 
         public <U> LazyCollectionHelper<T, U> map(Function<R, U> mapping) {
-            throw new UnsupportedOperationException();
+            return new LazyCollectionHelper<>(
+                    sourceList,
+                    listTransformation(mapping.andThen(Collections::singletonList)).compose(function));
         }
 
         public List<R> force() {
-            throw new UnsupportedOperationException();
+            return listTransformation(function).apply(sourceList);
+        }
+
+        private <U, V> Function<List<U>, List<V>> listTransformation(Function<U, List<V>> function) {
+            return list -> {
+                List<V> result = new ArrayList<>();
+                list.forEach(function.andThen(result::addAll)::apply);
+                return result;
+            };
         }
     }
 
@@ -40,13 +55,11 @@ class Exercise4 {
     void mapEmployeesToCodesOfLetterTheirPositionsUsingLazyFlatMapHelper() {
         List<Employee> employees = getEmployees();
 
-        List<Integer> codes = null;
-        // TODO              LazyCollectionHelper.from(employees)
-        // TODO                                  .flatMap(Employee -> JobHistoryEntry)
-        // TODO                                  .map(JobHistoryEntry -> String(position))
-        // TODO                                  .flatMap(String -> Character(letter))
-        // TODO                                  .map(Character -> Integer(code letter)
-        // TODO                                  .force();
+        List<Integer> codes = LazyCollectionHelper.from(employees)
+                                          .flatMap(Employee::getJobHistory)
+                                          .map(JobHistoryEntry::getPosition)
+                                          .flatMap(Exercise4::calcCodes)
+                                          .force();
         assertThat(codes, Matchers.contains(calcCodes("dev", "dev", "tester", "dev", "dev", "QA", "QA", "dev", "tester", "tester", "QA", "QA", "QA", "dev").toArray()));
     }
 
