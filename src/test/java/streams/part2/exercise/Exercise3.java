bb4a3b1ac918a88812fdca8c615288e0b2209a2d
elefus <elefus@yandex.ru>
2018-10-11 13:06:39 +0300

    Homework streams.part2.exercise3

---
+++ src/test/java/streams/part2/exercise/Exercise3.java
@@ -0,0 +1,28 @@
+package streams.part2.exercise;
+
+import org.junit.jupiter.api.Test;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.is;
+
+@SuppressWarnings({"unused", "ConstantConditions"})
+class Exercise3 {
+
+    @Test
+    void createLimitedStringWithOddNumbersSeparatedBySpaces() {
+        int countNumbers = 10;
+
+        String result = null;
+
+        assertThat(result, is("1 3 5 7 9 11 13 15 17 19"));
+    }
+
+    @Test
+    void extractEvenNumberedCharactersToNewString() {
+        String source = "abcdefghijklm";
+
+        String result = null;
+
+        assertThat(result, is("acegikm"));
+    }
+}
