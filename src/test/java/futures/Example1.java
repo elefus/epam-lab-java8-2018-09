5d8481b17500990469aad6d944aeaaf0d1d98a90
elefus <elefus@yandex.ru>
2018-10-19 17:15:36 +0300

    Code from 19_10_2018

--- src/test/java/futures/Example1.java
+++ src/test/java/futures/Example1.java
@@ -3,11 +3,20 @@ package futures;
 import lombok.SneakyThrows;
 import org.junit.jupiter.api.Test;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.CompletionException;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ForkJoinPool;
+import java.util.concurrent.Future;
 import java.util.concurrent.TimeUnit;
+import java.util.stream.IntStream;
 
 import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.in;
 import static org.hamcrest.Matchers.is;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
@@ -71,30 +80,126 @@ class Example1 {
 //        CompletableFuture<Integer> result = task1.thenCombine(task2, Integer::sum);
 //        result.thenAccept(System.out::println);
 
-        CompletableFuture.supplyAsync(() -> "21")
-                         .thenApply(Integer::valueOf)
-                         .thenCombine(CompletableFuture.supplyAsync(() -> 12), Integer::sum)
+        CompletableFuture.completedFuture(1)
+                         .thenApplyAsync(integer -> {
+                             System.out.println(Thread.currentThread());
+                             try {
+                                 TimeUnit.SECONDS.sleep(3);
+                             } catch (InterruptedException e) {
+                                 e.printStackTrace();
+                             }
+                             return integer + 1;
+                         })
+                         .thenCombine(CompletableFuture.completedFuture(2).thenApply(value -> {
+
+                             return value;
+                         }), (a, b) -> {
+                             System.out.println(Thread.currentThread());
+                             return Integer.sum(a, b);
+                         })
                          .thenAccept(System.out::println)
                          .join();
+
+//        CompletableFuture.supplyAsync(() -> "21")
+//                         .thenApply(Integer::valueOf)
+//                         .thenCombine(CompletableFuture.supplyAsync(() -> 12), Integer::sum)
+//                         .thenAccept(System.out::println)
+//                         .join();
     }
 
     @Test
-    void exceptionally() {
-        CompletableFuture.supplyAsync(() -> {
-                                if (true) {
-                                    throw new IllegalArgumentException();
-                                }
-                                return 123;
-                         })
-                         .exceptionally(throwable -> 42)
-                         .thenApply(value -> {
-//                             if (true) {
-//                                 throw new IllegalStateException();
-//                             }
-                             return value + 1;
-                         })
-//                         .exceptionally(throwable -> 50)
-                         .thenAccept(System.out::println)
-                         .join();
+    void exceptionally() throws InterruptedException {
+        CompletableFuture<Integer> task = CompletableFuture.supplyAsync(() -> {
+            try {
+                TimeUnit.SECONDS.sleep(3);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            System.out.println(Thread.currentThread());
+            return 123;
+        });
+
+        TimeUnit.SECONDS.sleep(1);
+
+        CompletableFuture.runAsync(() -> {
+            task.exceptionally(throwable -> {
+                System.out.println(Thread.currentThread());
+                return 42;
+            }).thenApply(value -> value + 1)
+                .thenAccept(System.out::println);
+        });
+
+        task.thenAccept(System.out::println);
+
+        task.thenApplyAsync(integer -> integer - 1)
+            .thenAccept(System.out::println);
+
+
+        TimeUnit.SECONDS.sleep(5);
+    }
+
+    @Test
+    void name() throws ExecutionException, InterruptedException {
+        ExecutorService service = new ForkJoinPool(1);
+        Future<Boolean> task = service.submit(() -> {
+            try {
+                service.submit(() -> {
+                    throw new RuntimeException();
+                }).get();
+            } catch (ExecutionException | InterruptedException ex) {
+                return false;
+            }
+            return true;
+        });
+
+        TimeUnit.SECONDS.sleep(2);
+        service.shutdownNow();
+
+        boolean result = task.get();
+
+        System.out.println(result);
+    }
+
+    @Test
+    void fixedThreadPool() throws ExecutionException, InterruptedException {
+        ExecutorService service = Executors.newFixedThreadPool(4);
+        Future<?> started = service.submit(() -> IntStream.rangeClosed(0, 1_000)
+                                                          .parallel()
+                                                          .mapToObj(i -> {
+                                                              System.out.println(Thread.currentThread());
+                                                              return String.valueOf(i);
+                                                          })
+                                                          .forEach(System.out::println));
+        service.shutdown();
+        started.get();
+    }
+
+    @Test
+    void fjp() throws ExecutionException, InterruptedException {
+        ExecutorService service = new ForkJoinPool();
+        Future<?> started = service.submit(() -> IntStream.rangeClosed(0, 1_000)
+                                                          .parallel()
+                                                          .mapToObj(i -> {
+                                                              System.out.println(Thread.currentThread());
+                                                              return String.valueOf(i);
+                                                          })
+                                                          .forEach(System.out::println));
+        service.shutdown();
+        started.get();
+    }
+
+    @Test
+    void mutableFuture() {
+        CompletableFuture<ArrayList<Integer>> result = CompletableFuture.supplyAsync(() -> new ArrayList<>(Arrays.asList(1, 2, 3)));
+
+        result.thenApply(list -> {
+            list.add(1);
+            return list;
+        }).thenAccept(System.out::println);
+
+        result.thenApply(list -> {
+            list.clear();
+            return list;
+        }).thenAccept(System.out::println);
     }
 }
